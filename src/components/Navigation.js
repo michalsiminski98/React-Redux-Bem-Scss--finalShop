49b8b80800347e6b0809eaa2df93c9d9c4091896
Michal Siminski <michal.siminski3@gmail.com>
2021-04-02 08:13:13 +0200

    first commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+const Navigation = () => {
+  return ( 
+    <nav className="navigation">
+      <ul className="navigation__list">
+        <li className="navigation__item">Strona główna</li>
+        <li className="navigation__item">Sklep</li>
+        <li className="navigation__item">Koszyk</li>
+      </ul>
+    </nav>
+   );
+}
+ 
+export default Navigation;
---
+++ src/components/Page.js
@@ -0,0 +1,9 @@
+const Page = () => {
+  return ( 
+    <section>
+      Page
+    </section>
+   );
+}
+ 
+export default Page;
