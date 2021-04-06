5a3088406d56749f7c1b8b934178c16bb46d48bd
Michal Siminski <michal.siminski3@gmail.com>
2021-04-06 13:17:14 +0200

    add gsap

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,31 @@
+import { TweenMax } from "gsap/all";
 import { useState } from "react";
 
 const Navigation = () => {
 
   const [isNavActive, setNavActive] = useState(false);
 
-  const handleToggleActive = () => {
+  const handleNavActive = () => {
     setNavActive(!isNavActive);
+    if (isNavActive) {
+      TweenMax.to('.navigation__list-mobile', { duration: 2, x: (window.innerWidth) });
+    } else {
+      TweenMax.to('.navigation__list-mobile', { duration: 1.1, x: 0, ease: "bounce.out" });
+  }
   };
 
   return ( 
     <>
+    <ul className="navigation__list-mobile">
+      <div
+      className= {isNavActive ? 'navigation__btn navigation__btn-in open' : 'navigation__btn navigation__btn-in'}
+      onClick={handleNavActive}>
+      <div className="navigation__burger"></div>
+      </div>
+        <li className="navigation__item-mobile">Strona główna</li>
+        <li className="navigation__item-mobile">Sklep</li>
+        <li className="navigation__item-mobile">Koszyk</li>
+    </ul>
     <nav className="navigation">
       <ul className="navigation__list">
         <li className="navigation__item">Strona główna</li>
@@ -19,14 +35,9 @@ const Navigation = () => {
       <div className="navigation__logo"></div>
       <div
       className= {isNavActive ? 'navigation__btn open' : 'navigation__btn'}
-      onClick={handleToggleActive}>
+      onClick={handleNavActive}>
       <div className="navigation__burger"></div>
       </div>
-      <ul className="navigation__list-mobile">
-        <li className="navigation__item-mobile">Strona główna</li>
-        <li className="navigation__item-mobile">Sklep</li>
-        <li className="navigation__item-mobile">Koszyk</li>
-      </ul>
     </nav>
     </>
    );
--- src/js/navShow.js
+++ src/js/navShow.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(){
+
   const navigation = document.querySelector('.navigation');
   window.addEventListener('scroll', () => {
     if(window.pageYOffset > window.innerHeight){
-      navigation.style.backgroundColor = "rgba(236, 236, 236, 0.6)";
+      navigation.style.backgroundColor = "rgba(236, 236, 236, 0.4)";
     } else {
       navigation.style.backgroundColor = "rgba(236, 236, 236, 0)";
     }
