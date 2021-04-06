4b807dc2c0993db1ee7b32d46d72dc2add6768ec
Michal Siminski <michal.siminski3@gmail.com>
2021-04-06 14:10:00 +0200

    prepare nav before react links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,7 +8,7 @@ const Navigation = () => {
   const handleNavActive = () => {
     setNavActive(!isNavActive);
     if (isNavActive) {
-      TweenMax.to('.navigation__list-mobile', { duration: 1.3, x: (window.innerWidth) });
+      TweenMax.to('.navigation__list-mobile', { duration: 1.3, x: (- window.innerWidth) });
     } else {
       TweenMax.to('.navigation__list-mobile', { duration: 1.6, x: 0, ease: "bounce.out" });
   }
@@ -17,11 +17,6 @@ const Navigation = () => {
   return ( 
     <>
     <ul className="navigation__list-mobile">
-      <div
-      className= {isNavActive ? 'navigation__btn navigation__btn-in open' : 'navigation__btn navigation__btn-in'}
-      onClick={handleNavActive}>
-      <div className="navigation__burger"></div>
-      </div>
         <li className="navigation__item-mobile">Strona główna</li>
         <li className="navigation__item-mobile">Sklep</li>
         <li className="navigation__item-mobile">Koszyk</li>
--- src/js/navShow.js
+++ src/js/navShow.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
   const navigation = document.querySelector('.navigation');
   window.addEventListener('scroll', () => {
-    if(window.pageYOffset > window.innerHeight){
+    if(window.pageYOffset > window.innerHeight && window.innerWidth > 767){
       navigation.style.backgroundColor = "rgba(236, 236, 236, 0.4)";
     } else {
       navigation.style.backgroundColor = "rgba(236, 236, 236, 0)";
