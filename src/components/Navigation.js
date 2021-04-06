6454ffa3cf252f7323bd44214c78b0f84f5e441e
Michal Siminski <michal.siminski3@gmail.com>
2021-04-06 11:41:41 +0200

    add nav specific functions

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,15 @@
+import { useState } from "react";
+
 const Navigation = () => {
+
+  const [isNavActive, setNavActive] = useState(false);
+
+  const handleToggleActive = () => {
+    setNavActive(!isNavActive);
+  };
+
   return ( 
+    <>
     <nav className="navigation">
       <ul className="navigation__list">
         <li className="navigation__item">Strona główna</li>
@@ -7,7 +17,18 @@ const Navigation = () => {
         <li className="navigation__item">Koszyk</li>
       </ul>
       <div className="navigation__logo"></div>
+      <div
+      className= {isNavActive ? 'navigation__btn open' : 'navigation__btn'}
+      onClick={handleToggleActive}>
+      <div className="navigation__burger"></div>
+      </div>
+      <ul className="navigation__list-mobile">
+        <li className="navigation__item-mobile">Strona główna</li>
+        <li className="navigation__item-mobile">Sklep</li>
+        <li className="navigation__item-mobile">Koszyk</li>
+      </ul>
     </nav>
+    </>
    );
 }
  
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './js/navShow';
 import './css/index.min.css';
 import App from './App';
 
