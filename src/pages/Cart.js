b9e93c7860a1430f3eeeac79b343c21c133ad355
Michal Siminski <michal.siminski3@gmail.com>
2021-04-08 11:50:38 +0200

    add cart style

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,8 +1,26 @@
+import tentNormal from '../images/tentNormal.jpg';
+
 const Cart = () => {
   return ( 
-    <div>
-      Cart
-    </div>
+    <section className="cart">
+      <div className="cart__items">
+        <div className="cart__item">
+          <img className="cart__itemPhoto" src={tentNormal} alt="tent"/>
+          <div className="cart__namesWrapper">
+          <p className="cart__itemName">Duap dupa</p>
+          <p className="cart__itemPrice">dupa: $123</p>
+          </div>
+        </div>
+      </div>
+      <div className="cart__sumPlace">
+        <div className="cart__sum">
+          <h4 className="cart__sumTitle">Podsumowanie</h4>
+          <p className="cart__sumtext">Ilość: <span className="cart__count"></span></p>
+          <p className="cart__sumtext">Cena: <span className="cart__price"></span></p>
+        <button className="cart__buyButton">Kup</button>
+        </div>
+      </div>
+    </section>
    );
 }
  
