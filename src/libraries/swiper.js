09be48263fe1f897827eb6c83c3058d77a6d134b
Michal Siminski <michal.siminski3@gmail.com>
2021-04-09 15:25:20 +0200

    rwd swipper

--- src/libraries/swiper.js
+++ src/libraries/swiper.js
@@ -1,8 +1,13 @@
 import Swiper from 'swiper/bundle';
 
 window.addEventListener('DOMContentLoaded', () =>{
+
+  let x;
+  if(window.innerWidth <= 1040) x=1;
+  else x=2;
+
 new Swiper('.swiper-container', {
-  slidesPerView: 2,
+  slidesPerView: x,
   zoom: false,
   navigation: {
     nextEl: '.swiper-button-next',
