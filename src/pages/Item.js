acb60929bbeb9123782bcf3408fde499dcaa4758
Michal Siminski <michal.siminski3@gmail.com>
2021-05-01 10:05:18 +0200

    added alert for adding item to cart

--- src/pages/Item.js
+++ src/pages/Item.js
@@ -2,28 +2,40 @@ import { connect } from "react-redux";
 import { addToCart } from "../actions/itemActions";
 
 const Item = (props) => {
-  
   // destructurization
-  const {addToCart} = props;
+  const { addToCart } = props;
   // getting data of current item from shop.js
-  const {id, name, img, price, describe} = props.location.state.productData.prod;
+  const {
+    id,
+    name,
+    img,
+    price,
+    describe,
+  } = props.location.state.productData.prod;
 
-  return ( 
+  const handleAddingItem = () => {
+    addToCart(id);
+    alert("Dodano do koszyka");
+  };
+
+  return (
     <section className="item">
       <h3 className="item__title">{name}</h3>
-      <img src={img} alt="namiot normalny" className="item__img"/>
+      <img src={img} alt="namiot normalny" className="item__img" />
       <div className="item__describes">
         <p className="item__describe">Cena: ${price}</p>
         <p className="item__describe item__describe-grey">Opis: {describe}</p>
-        <button onClick={() => addToCart(id)} className="item__add">Dodaj</button>
+        <button onClick={handleAddingItem} className="item__add">
+          Dodaj
+        </button>
       </div>
     </section>
-   );
-}
+  );
+};
 
 // add product to cart
-const mapDispatchToProps = dispatch => ({
-  addToCart: id => dispatch(addToCart(id)),
-})
- 
+const mapDispatchToProps = (dispatch) => ({
+  addToCart: (id) => dispatch(addToCart(id)),
+});
+
 export default connect(null, mapDispatchToProps)(Item);
